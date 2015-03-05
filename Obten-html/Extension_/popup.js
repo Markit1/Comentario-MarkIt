296a6dadb757f066796450ed35cf31e5e682e413
EderVs <eedervs.97@hotmail.com>
2015-03-04 18:37:01 -0600

    Poder desplazar el comentario a donde quieras

--- Obten-html/Extension_/popup.js
+++ Obten-html/Extension_/popup.js
@@ -1,20 +1,41 @@
 
+  var entero = 0;
+
   $("body").mousedown(function(e){
 
- if (e.srcElement)
-    tag = e.srcElement.tagName;
-  else if (e.target)
-      tag = e.target.tagName;
+    if (e.srcElement)
+        tag = e.srcElement.tagName;
+    else if (e.target)
+        tag = e.target.tagName;
     
       //Funcion que pide nombre de comentario que se usará como id y crea un nuevo elemento div en el DOM
-    $(tag).dblclick(function(){
-      var titulo = prompt("Agrega un titulo");
+  $(tag).dblclick(function(){
+      
+      var titulo = "comentario"+entero;
       var id = titulo.replace(/\s+/g,'');
-      id = id.toLowerCase();
-      $(this).append("<div id="+id+" class="+"markit-div"+"></div>")
+        id = id.toLowerCase();
+     $(this).append("<div id="+id+" class="+"markit-div"+"></div>")
+
       var comentario = prompt("Agrega un comentario");
-      $("#"+id+"").html(comentario);
+      var cantidad = comentario.length;
+        if (cantidad <= 60){
+          $("#"+id+"").html(comentario);
+            $(function() {
+              $( "#"+id+"").draggable();
+          });
+
+
       comentario = undefined;
       hola = u;
-    });  
+      }
+    
+    else{
+
+      alert("No se puede usar tantas palabras");
+    }
+
+
+entero++;
+
+  });  
 });
